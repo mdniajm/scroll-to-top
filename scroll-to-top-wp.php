1aa81de8707800661346e82c5631e1426fa2b338
mdniajm <mdniajm@gmail.com>
2023-05-21 18:34:31 +0600

    fix description

--- scroll-to-top-wp.php
+++ scroll-to-top-wp.php
@@ -10,7 +10,6 @@
  * Author URI:        https://mdniajmakhdum.me/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
  * Text Domain:       sstt
  * Domain Path:       /languages
  */
